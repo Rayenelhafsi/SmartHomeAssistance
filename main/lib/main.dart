86d00106be587a0d08afa30dc8503ed760392330
elhafsirayen <elhafsirayen@gmail.com>
2025-02-24 21:01:05 +0100

    "firebase config 24/02/2025 added dependecies "

--- main/lib/main.dart
+++ main/lib/main.dart
@@ -1,21 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:project/login.dart';
 
-void main()
-{
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
-
-class MyApp extends StatelessWidget
-{
- 
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context)
-  {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: login(),
-    );
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: login());
   }
 }
